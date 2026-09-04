e4e1fbef48b25558de6afc19dbe754d8a8641645
Yoshino <892333579@qq.com>
2026-09-05 02:08:54 +0800

    build: bootstrap Spring Boot application

---
+++ src/test/java/com/supermarket/SupermarketApplicationTest.java
@@ -0,0 +1,13 @@
+package com.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
+class SupermarketApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
